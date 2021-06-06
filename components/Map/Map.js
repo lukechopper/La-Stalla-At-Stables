764629fc9c85710c88e9313aa9feae5a128d187f
lukechopper <lukechopper22@gmail.com>
2021-06-06 12:43:40 +0100

    Removed directions because free trial has ended

--- components/Map/Map.js
+++ components/Map/Map.js
@@ -1,64 +1,14 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import GoogleMapReact from 'google-map-react';
 import LocationMarker from './LocationMarker';
-import { start } from 'nprogress';
 
 function Map(props) {
 
     const [showMarker, setShowMarker] = useState(false);
-    const [startPlace, setStartPlace] = useState(null);
 
-    useEffect(() => {
-        if(typeof window !== "undefined"){
-            window.navigator.geolocation.getCurrentPosition(setLocation, failLocation);
-        }
-    }, []);
-
-    const setLocation = (loc) => {
-        loc = loc.coords;
-        setStartPlace({lat: loc.latitude, lng: loc.longitude});
-    }
-
-    const failLocation = () => {
-        setStartPlace("FAIL");
-    }
-
-    const apiIsLoaded = (map, maps) => {
-        const directionsService = new google.maps.DirectionsService();
-        const directionsRenderer = new google.maps.DirectionsRenderer();
-        directionsRenderer.setMap(map);
-        directionsRenderer.setOptions({
-            preserveViewport: true
-        });
-        const origin = { lat: startPlace.lat, lng: startPlace.lng };
-        const destination = { lat: props.lat, lng: props.lng };
-  
-        directionsService.route(
-          {
-            origin: origin,
-            destination: destination,
-            travelMode: google.maps.TravelMode.DRIVING,
-          },
-          (result, status) => {
-            if (status === google.maps.DirectionsStatus.OK) {
-              directionsRenderer.setDirections(result);
-            } else {
-              console.error(`error fetching directions ${result}`);
-            }
-          }
-        );
-      };
 
     return (
-        startPlace !== "FAIL" && startPlace !== null ?
         <GoogleMapReact
-        bootstrapURLKeys={{key: process.env.NEXT_PUBLIC_GOOGLE_MAP}}
-        center={{lat: props.lat, lng: props.lng}}
-        zoom={18}
-        onClick={() => setShowMarker(false)}
-        yesIWantToUseGoogleMapApiInternals
-        onGoogleApiLoaded={({ map, maps }) => apiIsLoaded(map, maps)}>
-        </GoogleMapReact> : startPlace !== null ? <GoogleMapReact
         bootstrapURLKeys={{key: process.env.NEXT_PUBLIC_GOOGLE_MAP}}
         center={{lat: props.lat, lng: props.lng}}
         zoom={18}
@@ -71,7 +21,7 @@ function Map(props) {
             setShowMarker={setShowMarker}
             showMarker={showMarker}
         />
-        </GoogleMapReact> : null
+        </GoogleMapReact> 
     );
 }
 
