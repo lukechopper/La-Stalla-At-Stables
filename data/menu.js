4dad91b0c98eafaf2b114519a7b91ed5259653f3
lukechopper <lukechopper22@gmail.com>
2021-08-27 11:20:34 +0100

    New information

--- data/menu.js
+++ data/menu.js
@@ -2,40 +2,60 @@ export const data = [
     {
         title: "Starters",
          items: [
+            {
+                title: "Zuppa del Giorno",
+                price: "£6.95",
+                desc: "Soup of the day"
+            },
              {
                  title: "Bruschetta",
-                 price: "£6.00",
+                 price: "£8.95",
                  desc: "Tomatoes marinated in garlic, basil and olive oil served on crostini bread"
              },
              {
+                title: "Portobello mushroom",
+                price: "£8.95",
+                desc: "Portobello mushroom with goat cheese and mix herbs, served with rocket salad and balsamic dressing"
+            },
+            {
                 title: "Bianchetto",
                 price: "£6.00",
                 desc: "Whitebait served with home made tartar sauce and salad"
             },
             {
                 title: "Calamari Fritti",
-                price: "£6.00",
+                price: "£8.95",
                 desc: "Deep-fried calamari served with tartar sauce and salad"
             },
             {
+                title: "Scallops",
+                price: "£9.95",
+                desc: "Pan-fried scallops with king prawns and garlic butter"
+            },
+            {
                 title: "Gamberoni alla Griglia",
-                price: "£7.00",
-                desc: "King prawns grilled and served with either; garlic butter, cream, garlic or brandy sauce"
+                price: "£11.95",
+                desc: "Jumbo prawns grilled and served with garlic butter sauce"
+            },
+            {
+                title: "Gamberetti alla Crema di Formaggio",
+                price: "£9.95",
+                desc: "King prawns cooked in cream, garlic, white wine and cheese sauce"
+            },
+            {
+                title: "Pate di Fegato di Pollo",
+                price: "£7.95",
+                desc: "Chicken live pate served with crostini bread and chutney"
+            },
+            {
+                title: "Antipasto",
+                price: "£10.95",
+                desc: "Traditional first course italian meal with italian cured meats, cheese and salamis"
             },
             {
                 title: "Prosciutto e Mozzarella",
-                price: "£6.00",
+                price: "£8.95",
                 desc: "Mozzarella cheese wrapped with prosciutto, served with rocked leaf and balsamic dressing"
-            },
-            {
-                title: "Mussels",
-                price: "£6.00",
-                desc: "Mussels in tomato sauce"
-            },
-            {
-                title: "Portobello mushroom",
-                price: "£6.00",
-                desc: "Portobello mushroom with goat cheese and mix herbs, served with rocked salad and balsamic dressing"
             },
          ]
     },
@@ -44,28 +64,38 @@ export const data = [
          items: [
              {
                  title: "Pollo alla crema",
-                 price: "£11.00",
+                 price: "£16.95",
                  desc: "Chicken breast with mushrooms, cream and white wine sauce"
              },
              {
                 title: "Pollo alla cacciatora",
-                price: "£11.00",
+                price: "£16.95",
                 desc: "Chicken breast with pancetta, mushrooms, garlic, tomato and white wine sauce"
             },
             {
                 title: "Pollo gamberetti",
-                price: "£12.00",
+                price: "£16.95",
                 desc: "Chicken strips with prawns, mushrooms, garlic in white wine creamy sauce"
             },
             {
-                title: "Beef stroganoff",
-                price: "£13.00",
-                desc: "Strips of fillet stake, cooked with mushrooms, onion, garlic, brandy and creamy sauce"
+                title: "Pollo involtini",
+                price: "£16.95",
+                desc: "Chicken breast with gorgonzola, wrapped with parma ham, served in creamy white wine sauce"
             },
             {
-                title: "Chicken stroganoff",
-                price: "£13.00",
-                desc: "Chicken strips, cooked with mushrooms, onion, garlic, brandy and creamy sauce"
+                title: "Vitelo Milanese",
+                price: "£17.95",
+                desc: "Veal coated with bread crumbs, served with tomato, spaghetti"
+            },
+            {
+                title: "Vitelo alla marsala",
+                price: "£17.95",
+                desc: "Veal, served with marsala, wine, mushrooms, creamy sauce"
+            },
+            {
+                title: "Saltimboca di vitello alla romana",
+                price: "£17.95",
+                desc: "Veal, lined with proscutto and sage in wine sauce"
             }
          ]
     },
@@ -74,18 +104,18 @@ export const data = [
          items: [
              {
                  title: "Risotto con Funghi and Asparagus",
-                 price: "£10.00",
-                 desc: "Authentically delicious risotto with the woodsy, flavour of mushrooms and asparagus"
+                 price: "£14.95",
+                 desc: "Authentically risotto with the woodsy, mushrooms, asparagus"
              },
              {
                 title: "Risotto frutti di Mare",
-                price: "£12.00",
+                price: "£16.95",
                 desc: "Risotto with seafood, garlic, tomato, with wine sauce"
             },
             {
                 title: "Risotto con Funghi and chicken",
-                price: "£11.00",
-                desc: "Authentically delicious risotto with the woodsy, flavour of mushrooms chicken"
+                price: "£14.95",
+                desc: "Authentically risotto with the woodsy mushrooms and chicken"
             }
          ]
     },
@@ -94,13 +124,28 @@ export const data = [
          items: [
              {
                  title: "Spigola",
-                 price: "£11.00",
-                 desc: "Seabass fillet pan-fried, served with garlic butter on marinara sauce with prawns and mussels"
+                 price: "£18.95",
+                 desc: "Seabass fillet pan-fried, garlic butter on marinara sauce with prawns and mussels"
              },
              {
-                title: "Salmon",
-                price: "£11.00",
-                desc: "Creamy garlic Tuscan salmon, with spinach, sun dried tomatoes and cheese"
+                title: "Pesce spada",
+                price: "£18.95",
+                desc: "Swordfish with fresh tomato relish"
+            },
+            {
+                title: "Salmone",
+                price: "£17.95",
+                desc: "Creamy garlic toscan salmon, with spinach, sun dried tomatoes and cheese"
+            },
+             {
+                title: "Coda di rospo",
+                price: "£18.95",
+                desc: "Monkfish wrapped in pancetta served with creamy garlic sauce"
+            },
+            {
+                title: "Mescolare il pesce",
+                price: "£29.95",
+                desc: "Mix fish board (chef choice)"
             }
          ]
     },
@@ -109,29 +154,44 @@ export const data = [
          items: [
              {
                  title: "Fillet Rossini",
-                 price: "8oz £16.00",
-                 desc: "Fillet stake topped with pate and madeira wine sauce"
+                 price: "8oz £24.95",
+                 desc: "Fillet steak topped with pate and madeira wine sauce"
              },
              {
                 title: "Fillet al peppe",
-                price: "8oz £16.00",
+                price: "8oz £24.95",
                 desc: "Filled steak with green peppercorn sauce"
             },
             {
                 title: "Fillet al porto",
-                price: "8oz £16.00",
+                price: "8oz £24.95",
                 desc: "Fillet steak with port, mushrooms, creamy sauce"
             },
             {
                title: "Fillet al gorgonzola",
-               price: "8oz £16.00",
+               price: "8oz £24.95",
                desc: "Fillet steak with creamy gorgonzola, cheese sauce"
            },
            {
-            title: "Sirloin steak",
-            price: "£14.00",
-            desc: "Sirloin stake with peppercorn sauce or plain"
-        }
+                title: "Chateaubriand",
+                price: "£49.95",
+                desc: "Thickest part of the fillet steak"
+            },
+            {
+                title: "Beef stroganoff",
+                price: "£21.95",
+                desc: "Strips of fillet stakem cooked with mushrooms, onion, garlic, brandy and creamy sauce"
+            },
+            {
+                title: "Rib eye steak",
+                price: "£23.95",
+                desc: "Steak with natural fats which caramelise when cooked adding depth of flavour"
+            },
+            {
+                title: "Bourguinon di manzo",
+                price: "£21.95",
+                desc: "Strips of beef cooked with mushrooms, onions, garlic and pancetta in red whine sauce"
+            },
          ]
     },
     {
@@ -139,74 +199,112 @@ export const data = [
          items: [
              {
                  title: "Spaghetti Bolognese",
-                 price: "£11.00",
+                 price: "£13.95",
                  desc: "Spaghetti cooked with traditional homemade Bolognese sauce"
              },
              {
                 title: "Penne arrabbiata",
-                price: "£10.00 or with chicken £11.00",
+                price: "£12.95 or with chicken £14.95",
                 desc: "Penne cooked in tomato, garlic and chilli sauce"
             },
             {
                 title: "Spaghetti Carbonara",
-                price: "£11.00",
+                price: "£14.95",
                 desc: "Spaghetti cooked with pancetta, egg yolk, black pepper and creamy cheese sauce"
             },
             {
+                title: "Tortellini con Spinaci e Ricotta",
+                price: "£14.95",
+                desc: "Pasta stuffed with ricotta and spinach, cooked with cream and cheese sauce"
+            },
+            {
                title: "Lasagne al Forno",
-               price: "£11.00",
-               desc: "Italian pasta, layers filled with Bolognese and béchamel sauce topped with Italian cheese"
+               price: "£15.95",
+               desc: "Italian pasta with Bolognese and béchamel sauce topped with italian cheese"
            },
            {
+            title: "Linguine con Aragosta e Pomodorini",
+            price: "£25.95",
+            desc: "Linguine pasta with lobster, cherry tomatoes, garlic, and basil sauce"
+        },
+           {
             title: "Tagliatelle frutti di Mare",
-            price: "£12.00",
-            desc: "Tagliatelle pasta cooked with seafood, chilli, white wine and tomato sauce"
+            price: "£16.95",
+            desc: "Tagliatelle pasta, seafood, chilli, garlic, chilli, white wine, tomato sauce"
         },
         {
-            title: "Spaghetti amatriciana",
-            price: "£12.00",
-            desc: "Spaghetti cooked with bacon, tomato, onion, garlic"
-        },
-        {
-         title: "Spaghetti napolitan",
-         price: "£11.00",
-         desc: "Spaghetti cooked with fresh basil, tomato sauce, onion, garlic"
-     },
+            title: "Linguine pollo gamboretti",
+            price: "£16.95",
+            desc: "Linguine with chicken and prawns"
+        }
          ]
     },
     {
-        title: "Pizzas",
+        title: "Desserts",
          items: [
              {
-                 title: "Pizza margherita",
-                 price: "£10.00",
-                 desc: "Classic tomato sauce and mozzarella cheese"
+                 title: "Tiramisu",
+                 price: "£5.95",
+                 desc: "Savoiardi sponge coaked in coffee and covered with mascarpone cream"
              },
              {
-                title: "Pizza prosciutto",
-                price: "£11.00",
-                desc: "Tomato sauce, mozzarella, ham, mushrooms"
+                title: "Lemon cheesecake",
+                price: "£5.95",
+                desc: "This Italian cheesecake is made light and fluffy with the addition of ricotta cheese"
             },
             {
-                title: "Pizza diavola",
-                price: "£11.00",
-                desc: "Tomato sauce, mozzarella, spicy salami, onions, peppers and chilli"
+                title: "Fudge cake",
+                price: "£5.95",
+                desc: "Warm chocolate pudding with vanilla ice cream"
             },
             {
-               title: "Pizza Frutti Di Mare",
-               price: "£13.50",
-               desc: "Tomato sauce, mozzarella, mix seafood garlic, chilli"
+               title: "Profiteroles",
+               price: "£5.95",
+               desc: "The ultimate impressive dessert topped with chocolate"
            },
            {
-            title: "Pizza napolitana ",
-            price: "£11.00",
-            desc: "Tomato sauce, anchovy and capres"
+            title: "Limoncello pudding",
+            price: "£6.95",
+            desc: "Lemon sorbet infused with limoncello and light meringue"
         },
         {
-            title: "Calzone carne picante",
-            price: "£13.50",
-            desc: "Bolognese, mush-rooms, pepperoni, chilli, mozzarella and tomato sauce"
-        }
+            title: "Affogato",
+            price: "£6.95",
+            desc: "Italian vanilla ice cream, espresso, ameretto liquer and amaretti biscuits"
+        },
+        {
+            title: "Cream brule",
+            price: "£5.95",
+        },
+        {
+            title: "Tartufo scuro",
+            price: "£6.95",
+            desc: "Italian truffle creamy zabaglione ice cream surrounded by a layer of chocolate hazelnut ice cream covered in coco powder"
+        },
+        {
+            title: "Tartufo bianco",
+            price: "£6.95",
+            desc: "Italian truffle shaped hazelnut and coffe ice cream rolled in crushed meringue"
+        },
+        {
+            title: "Cannolo siciliano",
+            price: "£6.95",
+            desc: "Tubular pastry shellstuffed with sweetened filling of whipped ricotta containing nuts and chocolate"
+        },
+        {
+            title: "Torta della Nonna (Grandmothers cake)",
+            price: "£5.95",
+            desc: "Delicious Italian apple cake that everyones nanna knows, served with custard or ice cream"
+        },
+        {
+            title: "Cheese board",
+            price: "£11.80"
+        },
+        {
+            title: "Ice cream",
+            price: "£5.95",
+            desc: "A mix of Italian ice cream"
+        },
          ]
     },
     {
@@ -214,27 +312,35 @@ export const data = [
          items: [
              {
                  title: "Garlic bread",
-                 price: "£4.00",
+                 price: "£4.95",
              },
              {
                 title: "Garlic bread with cheese",
-                price: "£5.00",
+                price: "£5.95",
+            },
+            {
+                title: "Rocked and parmesan cheese salad",
+                price: "£3.95",
             },
             {
                 title: "Garden salad",
-                price: "£2.00",
+                price: "£4.95",
             },
             {
                 title: "Tomato and onion salad",
-                price: "£2.00",
-            },
-            {
-                title: "French fries",
-                price: "£2.00",
+                price: "£3.95",
             },
             {
                 title: "Zucchini fritti",
-                price: "£3.00",
+                price: "£3.95",
+            },
+            {
+                title: "French fries",
+                price: "£3.95",
+            },
+            {
+                title: "Asparagus with butter",
+                price: "£5.95",
             },
          ]
     }
